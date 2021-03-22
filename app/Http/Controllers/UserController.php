e79d4fcd1d67fc6cb7ebf78675b35876c70ab3f8
hadimajid-softenica <hadimajidsoftenica@gmail.com>
2021-03-22 14:14:15 +0500

    22-3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -661,7 +661,7 @@ class UserController extends Controller
         $cart=null;
         $totalPrice=0;
         if($user->cart){
-            $cart=CartItems::where('cart_id',$user->cart->id)->with(['product:id,Name,SalePrice,PromotionCheck,ProductNumber','product.nextGenImages:ProductId,name'])->get();
+            $cart=CartItems::where('cart_id',$user->cart->id)->with(['product:id,Name,SalePrice,PromotionCheck,ProductNumber','product.nextGenImages:ProductId,name','product.inventory.eta'])->get();
 //            $prices=$cart->map(function ($value){
 //                return $value->quantity*$value->product->PromotionPrice;
 //            });
