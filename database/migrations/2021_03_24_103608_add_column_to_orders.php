4141c67382806ac6bba733cb740ad546ad1018b6
hadimajid-softenica <hadimajidsoftenica@gmail.com>
2021-03-24 15:44:30 +0500

    24-3

---
+++ database/migrations/2021_03_24_103608_add_column_to_orders.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnToOrders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->boolean('ship')->after('status')->nullable()->default(false);
+            $table->text('shipping_address')->after('ship')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn(['shipping_address','ship']);
+        });
+    }
+}
