f1c868106dbd19453e5612ef1dd7da7fa30894fa
hadimajid-softenica <hadimajidsoftenica@gmail.com>
2021-03-31 16:15:41 +0500

    30 March

--- routes/api.php
+++ routes/api.php
@@ -116,6 +116,7 @@ use Illuminate\Support\Facades\Route;
                 Route::post('name',[AdminController::class,'getProductName']);
                 Route::post('hide',[AdminController::class,'hideProduct']);
                 Route::post('new',[AdminController::class,'newProduct']);
+                Route::post('featured',[AdminController::class,'featuredProduct']);
                 Route::post('hot',[AdminController::class,'hot']);
                 Route::post('/{id}',[AdminController::class,'getProductById']);
                 Route::delete('/image/{id}',[AdminController::class,'deleteImageById']);
