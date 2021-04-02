3f5a83c5224547a5704b27f67a13a376ae86d273
hadimajid-softenica <hadimajidsoftenica@gmail.com>
2021-04-02 15:39:21 +0500

    2 April

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BillingAddress;
 use App\Models\Cart;
 use App\Models\CartItems;
+use App\Models\ShippingAddress;
 use App\Models\WebsiteSettings;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Response;
 use Stripe\Checkout\Session;
 use Stripe\Stripe;
@@ -24,80 +27,130 @@ class PaymentController extends Controller
         $request->validate([
            'success_url'=>'required|url',
            'cancel_url'=>'required|url',
+            'notes'=>'nullable',
+            'ship'=>'nullable',
+            'update'=>'nullable',
+            'shipping_address'=>'required_if:ship,1|array',
+            'coupon'=>'nullable',
+            'shipping_address.name'=>'required_if:ship,1',
+            'shipping_address.company_name'=>'nullable',
+            'shipping_address.street_address'=>'required_if:ship,1',
+            'shipping_address.city'=>'required_if:ship,1',
+            'shipping_address.state'=>'required_if:ship,1',
+            'shipping_address.zip'=>'required_if:ship,1',
+            'shipping_address.country'=>'required_if:ship,1',
+            'shipping_address.phone'=>'required_if:ship,1',
+            'shipping_address.email'=>'required_if:ship,1|email',
+            'billing_address.name'=>'required',
+            'billing_address.company_name'=>'nullable',
+            'billing_address.street_address'=>'required',
+            'billing_address.city'=>'required',
+            'billing_address.state'=>'required',
+            'billing_address.zip'=>'required',
+            'billing_address.country'=>'required',
+            'billing_address.phone'=>'required',
+            'billing_address.email'=>'required|email',
         ]);
-        $user=auth()->guard('user')->user();
-        $cart=$user->cart;
-        if(!$cart){
-            return false;
-        }
-        $items=CartItems::where('cart_id',$cart->id)->with('product.nextGenImages')->get();
-        if(!$items){
-            return false;
-        }
-        $checkoutItem=[];
-        $tax_rate = \Stripe\TaxRate::create([
-            'display_name' => 'Sales Tax',
-            'inclusive' => false,
-            'percentage' => WebsiteSettings::first()->tax?WebsiteSettings::first()->tax:0,
-            'country' => 'US',
-            'state' => 'CA',
-            'jurisdiction' => 'US - CA',
-            'description' => 'CA Sales Tax',
-        ]);
-        foreach ($items as $key=>$item){
-            $checkoutItem[$key]['price_data']['currency']='usd';
-            $checkoutItem[$key]['price_data']['unit_amount']=$item->product->PromotionPrice*100;
-            $checkoutItem[$key]['price_data']['product_data']['name']=$item->product->Name;
-            $checkoutItem[$key]['quantity']=$item->quantity;
-            $checkoutItem[$key]['tax_rates']=[$tax_rate->id];
-            $checkoutItem[$key]['price_data']['product_data']['images']=[$_SERVER['APP_URL'].'/'.$item->product->nextGenImages->pluck('name')[0]];
+        try {
+            DB::beginTransaction();
+            $user=auth()->guard('user')->user();
+            $update=$request->input('update');
+            $ship=$request->input('ship');
+            if($update===1){
+                $temp=$request->input('shipping_address');
+                if(empty($user->shippingAddress)){
+                    $shipping=new ShippingAddress();
+                    $temp=array_merge($temp,['user_id'=>$user->id]);
+                    $shipping->fill($temp);
+                    $shipping->save();
+                }else{
+                    $shipping=$user->shippingAddress;
+                    $shipping->fill($temp);
+                    $shipping->save();
+                }
+            }
+            if(empty($user->billingAddress)){
+                $temp=$request->input('billing_address');
+                $billingAddress=new BillingAddress();
+                $temp=array_merge($temp,['user_id'=>$user->id]);
+                $billingAddress->fill($temp);
+                $billingAddress->save();
+            }else{
+                $temp=$request->input('billing_address');
+                $billingAddress=$user->billingAddress;
+                $billingAddress->fill($temp);
+                $billingAddress->save();
+            }
+
+            if($ship===1){
+                $add=$request->input('shipping_address');
+            }else{
+                $add=$request->input('billing_address');
+            }
+            $cart=$user->cart;
+            if(!$cart){
+                return false;
+            }
+            $items=CartItems::where('cart_id',$cart->id)->with('product.nextGenImages')->get();
+            if(!$items){
+                return false;
+            }
+            $checkoutItem=[];
+            $tax_rate = \Stripe\TaxRate::create([
+                'display_name' => 'Sales Tax',
+                'inclusive' => false,
+                'percentage' => WebsiteSettings::first()->tax?WebsiteSettings::first()->tax:0,
+                'country' => 'US',
+                'state' => 'CA',
+                'jurisdiction' => 'US - CA',
+                'description' => 'CA Sales Tax',
+            ]);
+            foreach ($items as $key=>$item){
+                $checkoutItem[$key]['price_data']['currency']='usd';
+                $checkoutItem[$key]['price_data']['unit_amount']=$item->product->PromotionPrice*100;
+                $checkoutItem[$key]['price_data']['product_data']['name']=$item->product->Name;
+                $checkoutItem[$key]['quantity']=$item->quantity;
+                $checkoutItem[$key]['tax_rates']=[$tax_rate->id];
+                $checkoutItem[$key]['price_data']['product_data']['images']=[$_SERVER['APP_URL'].'/'.$item->product->nextGenImages->pluck('name')[0]];
 //                $item->product->nextGenImages->pluck('name')->map(function ($value){
 //                    if($value){
 //                        return $_SERVER['APP_URL'].'/'.$value;
 //                    }
 //                });
-        }
-//            return $checkoutItem;
-//        $checkoutItem=[
-//            [
-//                'price_data' => [
-//                    'currency' => 'usd',
-//                    'unit_amount' => 2000,
-//                    'product_data' => [
-//                        'name' => 'Stubborn Attachments',
-//                        'images' => ["https://i.imgur.com/EHyR2nP.png"],
-//                    ],
-//                ],
-//                'quantity' => 1,
-//            ],[
-//                'price_data' => [
-//                    'currency' => 'usd',
-//                    'unit_amount' => 2000,
-//                    'product_data' => [
-//                        'name' => 'Stubborn Attachments',
-//                        'images' => ["https://i.imgur.com/EHyR2nP.png"],
-//                    ],
-//                ],
-//                'quantity' => 1,
-//            ]
-//        ];
+            }
 
-        $checkout_session = Session::create([
-            'payment_method_types' => ['card'],
-            'shipping_rates' => ['shr_1IbjlWA0smjrwOKOJuGhAZBy'],
-            'shipping_address_collection' => [
-                'allowed_countries' => ['US', 'CA'],
-            ],
-            'client_reference_id'=>$user->id,
-            'line_items' => $checkoutItem,
-            'mode' => 'payment',
-            'metadata'=>[
-                'user_id'=>  $user->id,
-                'cart_id'=>$cart->id
-            ],
-            'success_url' => $request->input('success_url'),
-            'cancel_url' => $request->input('cancel_url'),
-        ]);
-        return Response::json(['id' => $checkout_session->id],200);
+            $checkout_session = Session::create([
+                'payment_method_types' => ['card'],
+                'shipping_rates' => ['shr_1IbjlWA0smjrwOKOJuGhAZBy'],
+                'shipping_address_collection' => [
+                    'allowed_countries' => ['US', 'CA'],
+                ],
+                'client_reference_id'=>$user->id,
+                'line_items' => $checkoutItem,
+                'shipping'=>[
+                    'name'=>$add['name'],
+                    'address'=>[
+                        'city'=>$add['city'],
+                        'country'=>$add['country'],
+                        'line1'=>$add['street_address'],
+                        'line2'=>$add['street_address'],
+                        'postal_code'=>$add['zip'],
+                        'state'=>$add['state'],
+                    ]
+                ],
+                'mode' => 'payment',
+                'metadata'=>[
+                    'user_id'=>  $user->id,
+                    'cart_id'=>$cart->id,
+                ],
+                'success_url' => $request->input('success_url'),
+                'cancel_url' => $request->input('cancel_url'),
+            ]);
+            DB::commit();
+            return Response::json(['id' => $checkout_session->id],200);
+        }catch (\Exception $ex){
+            DB::rollback();
+            return Response::json(['error'=>$ex->getMessage()],422);
+        }
     }
 }
