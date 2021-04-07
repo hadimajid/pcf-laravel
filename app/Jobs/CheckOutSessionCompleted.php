cf8aed607317d603e2ab7ef3e6cc0e76c3a03258
hadimajid-softenica <hadimajidsoftenica@gmail.com>
2021-04-07 18:51:55 +0500

    7 April

--- app/Jobs/CheckOutSessionCompleted.php
+++ app/Jobs/CheckOutSessionCompleted.php
@@ -20,7 +20,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Response;
 use Spatie\WebhookClient\Models\WebhookCall;
